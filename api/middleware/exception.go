37c445f7c3d9f8d6e764d2e45665b84f1c00879a
bidianqing <bidianqing@qq.com>
2024-03-10 17:22:57 +0800

    *

--- api/middleware/exception.go
+++ api/middleware/exception.go
@@ -1,17 +1,18 @@
 package middleware
 
 import (
-	"fmt"
+	"runtime/debug"
 
+	"github.com/bidianqing/go-use-gin/internal/pkg/log"
 	"github.com/gin-gonic/gin"
 )
 
-// 异常处理中间件 TODO 记录更相信的日志信息
+// 异常处理中间件 TODO 记录更详细的日志信息，包括堆栈信息
 func ExceptionHandler(ctx *gin.Context) {
 	defer func() {
 		err := recover()
 		if err != nil {
-			fmt.Println("err错误信息：", err)
+			log.Logger.Errorf("%s  %s", err, string(debug.Stack()))
 			ctx.JSON(500, gin.H{"success": false, "message": "程序开小差了", "data": nil})
 			ctx.Abort()
 		}
