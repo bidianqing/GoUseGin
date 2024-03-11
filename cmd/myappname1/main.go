286402ca14d5c366d93e8faa986f6bd32ebf1ee3
bidianqing <bidianqing@qq.com>
2024-03-11 22:44:28 +0800

    *

--- cmd/myappname1/main.go
+++ cmd/myappname1/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	router "github.com/bidianqing/go-use-gin/api"
 	"github.com/bidianqing/go-use-gin/api/middleware"
@@ -9,14 +11,20 @@ import (
 	"github.com/bidianqing/go-use-gin/internal/pkg/redis"
 	static "github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	jsoniter "github.com/json-iterator/go"
 )
 
 func main() {
 	// 获取运行环境 Development  Staging or Production
-	var environmentName string
-	flag.StringVar(&environmentName, "env", "Development", "环境变量")
+	environmentName := getEnvironmentName()
 
+	var flagEnvironmentName string
+	flag.StringVar(&flagEnvironmentName, "env", "", "环境变量")
 	flag.Parse()
+	if flagEnvironmentName != "" {
+		environmentName = flagEnvironmentName
+	}
+
 	isProduction := environmentName == "Production"
 	if isProduction {
 		gin.SetMode(gin.ReleaseMode)
@@ -37,5 +45,26 @@ func main() {
 
 	router.Map(app)
 
+	fmt.Println("Hosting Environment: ", environmentName)
 	app.Run()
+}
+
+func getEnvironmentName() string {
+	var environmentName string
+	data, err := os.ReadFile("./launchSettings.json")
+	if err == nil {
+		m := make(map[string]interface{})
+		jsoniter.Unmarshal(data, &m)
+		environmentName = m["GIN_ENVIRONMENT"].(string)
+	}
+
+	if environmentName == "" {
+		environmentName = os.Getenv("GIN_ENVIRONMENT")
+	}
+
+	if environmentName == "" {
+		environmentName = "Production"
+	}
+
+	return environmentName
 }
