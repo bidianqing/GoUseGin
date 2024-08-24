b3c10bd3ea613375a592c204a91f0e7fdbed0d3f
三大神捕之四 <bidianqing@qq.com>
2024-08-24 09:19:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./publish cmd/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./publish/ cmd/main.go
 mkdir ./publish/configs
 cp -r configs/*.json ./publish/configs
 cp -r wwwroot/ ./publish
